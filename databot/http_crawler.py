f0d2a920d702164e199ff67fab1e2b3682ffb2da
Surendra <suren@surendradangol.com.np>
2015-04-01 14:48:42 +0200

    Add Twitter API client

---
+++ databot/http_crawler.py
@@ -0,0 +1,116 @@
+import json
+import oauth2 as oauth
+import urllib2 as urllib
+from pymongo import MongoClient
+import sys,traceback
+import time
+import ConfigParser
+
+class TwitterAPIClient:
+
+	def __init__(self):
+	
+		Config = ConfigParser.ConfigParser()
+
+		Config.read("api_token.ini")
+
+		self.api_key = Config.get("TwitterApiToken", "api_key") 
+		self.api_secret = Config.get("TwitterApiToken", "api_secret") 
+		self.access_token_key = Config.get("TwitterApiToken", "access_token_key") 
+		self.access_token_secret = Config.get("TwitterApiToken", "access_token_secret") 
+
+		self._debug = 0
+
+		self.oauth_token    = oauth.Token(key=self.access_token_key, secret=self.access_token_secret)
+		self.oauth_consumer = oauth.Consumer(key=self.api_key, secret=self.api_secret)
+
+		self.signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
+
+		self.http_method = "GET"
+
+		self.http_handler  = urllib.HTTPHandler(debuglevel=self._debug)
+		self.https_handler = urllib.HTTPSHandler(debuglevel=self._debug)
+
+	'''
+	Construct, sign, and open a twitter request
+	using the hard-coded credentials above.
+	'''
+	def twitterRequest(self, url, method, parameters=[]):
+	  self.req = oauth.Request.from_consumer_and_token(self.oauth_consumer,
+												 token=self.oauth_token,
+												 http_method=method,
+												 http_url=url, 
+												 parameters=parameters)
+
+	  self.req.sign_request(self.signature_method_hmac_sha1, self.oauth_consumer, self.oauth_token)
+
+	  headers = self.req.to_header()
+
+	  if method == "POST":
+		encoded_post_data = self.req.to_postdata()
+	  else:
+		encoded_post_data = None
+		url = self.req.to_url()
+
+	  opener = urllib.OpenerDirector()
+	  opener.add_handler(self.http_handler)
+	  opener.add_handler(self.https_handler)
+
+	  response = opener.open(url, encoded_post_data)
+
+	  return response
+
+	def search(self, keyword):
+		url = "https://api.twitter.com/1.1/search/tweets.json?q="+ urllib.quote(keyword)+"&count=3000&lang=en"
+		response = self.twitterRequest(url, "GET");
+		return response
+
+	def fetchUserTimeline(self, screenHandle):
+		url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="+screenHandle+"&count=3000"
+		response = self.twitterRequest(url, "GET");
+		return response
+
+	def fetchPublicStream(self, lang="en", track="twitter"):
+		url = "https://stream.twitter.com/1.1/statuses/filter.json?language="+lang+"&track="+urllib.quote(track)
+		response = self.twitterRequest(url, "GET");
+		return response
+	
+	def fetchTrends(self, woeid=1):
+		url = "https://api.twitter.com/1.1/trends/place.json?id="+str(woeid)
+		response = self.twitterRequest(url, "GET");
+		return response
+
+	def saveStream(self, urlresponse, collection):
+		mclient = MongoClient()
+		dbcollection = mclient.twitter[collection]
+		for r in urlresponse:
+			try:
+				r = json.loads(r.strip())
+				dbcollection.insert(r)
+			except:
+				pass
+		
+	def getTweetsFromUsers(self, accounts=[]):
+		mclient = MongoClient()
+		db = mclient.twitter
+		userdb = db.usersTmp
+
+		if len(accounts) == 0:
+			accounts = ["barackobama", "arstechnica", "beINSPORTS", "vevo", "cosmopolitan"]
+
+		for acc in accounts:
+			result = self.fetchUserTimeline(acc)
+
+			count = 0
+			#f = open("tweets.txt", "w")
+			for l in result:
+				l = json.loads(l.strip())
+				userdb.insert(l)
+				#f.write(l)
+				count += 1;
+				print l
+
+if __name__ == "__main__":
+	tc = TwitterAPIClient()	
+	tc.getTweetsFromUsers()
+
