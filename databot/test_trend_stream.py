49b15901a10e958f444cf0c66139e57466b48de2
Surendra <suren@surendradangol.com.np>
2015-04-01 14:51:32 +0200

    Get tweet stream for current trends

---
+++ databot/test_trend_stream.py
@@ -0,0 +1,37 @@
+import http_crawler
+import json
+import urllib2 as urllib
+
+tc = http_crawler.TwitterAPIClient()	
+
+#for France WOEID
+#trendresp = tc.fetchTrends(23424819)
+
+#for the world
+trendresp = tc.fetchTrends(1)
+
+trends = []
+tr = ""
+for r in trendresp:
+	try:
+		tr = json.loads(r)
+	except:
+		pass
+
+tr = tr[0]
+for trend in tr['trends']:
+	try:
+		trendstr = str(trend['name'])
+		trendstr = trendstr.replace("#", "")
+		trends.append(trendstr)
+	except:
+		pass
+
+trendstr = ",".join(trends)
+
+print "============================================"
+print trendstr
+print "============================================"
+
+res = tc.fetchPublicStream("en", trendstr)
+tc.saveStream(res, "trends")
