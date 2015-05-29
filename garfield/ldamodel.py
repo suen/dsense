7c4324ee381b3967faa179d721b4274016e9260f
Surendra <suren@surendradangol.com.np>
2015-05-29 14:54:05 +0200

    def enrichDoc()

--- garfield/ldamodel.py
+++ garfield/ldamodel.py
@@ -6,16 +6,18 @@ from pymongo import MongoClient
 
 
 class LDAModel:
-	def __init__(self, dictionary):
+	def __init__(self, name, dictionary):
+		self.name = name
 		self.lda = models.LdaModel(id2word=dictionary, num_topics=200) 
 	
-	def trainModel(self, docs):
+	def trainModel(self, stream):
 		#tfidf = models.TfidfModel(docs);
 		#self.corpus_tfidf = tfidf[docs]
-		self.lda.update(docs)
+		self.lda.update(stream) #let's assume this works
 
 	def query(self, doc):
 		return self.lda[doc]
+
 	
 class TwitterStream:
 	def __init__(self):
@@ -48,9 +50,26 @@ class TwitterStream:
 		self.initCursor()
 		for d in self.cursor:
 			yield self.dictionary.doc2bow(self.clean(d['text']).lower().split())
+
+	#need lazy evaluation for this one
+	def enrichDoc(self, model):
+		enrichedStream = []
+		self.initCursor()
+		for d in self.cursor:
+			did =  self.dictionary.doc2bow(self.clean(d['text']).lower().split())
+			res = model.query(did)
+			
+			td = []
+			for tt in res:
+				td.append((model.name + "-" + str(tt[0]), tt[1]))
+				
+			d['topic'] = td
+			print d['text'] + " = ", d['topic']
+		return enrichedStream
+	
 	
 mg = TwitterStream()
-lda = LDAModel(mg.dictionary)
+lda = LDAModel("model1", mg.dictionary)
 
 mg.query("nepal", 0, 10)
 mgg = lda.trainModel(mg)
@@ -58,7 +77,6 @@ mgg = lda.trainModel(mg)
 mg.query("nepal", 10, 10)
 mgg = lda.trainModel(mg)
 
-mg.query("nepal", 20, 10)
-for x in mg:
-	print lda.query(x) 
+mg.query("nepal", 0, 20)
+encriched = mg.enrichDoc(lda)
 
