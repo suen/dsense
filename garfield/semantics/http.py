5e66d119d73dc895c36c9bcd7d1ebf24b59190df
Surendra <suren@surendradangol.com.np>
2015-06-11 17:46:11 +0200

    Add /feedback and /?getTopic= , /?getDiction=

--- garfield/semantics/http.py
+++ garfield/semantics/http.py
@@ -1,28 +1,69 @@
 import web
 from semantics import LDAModel 
 import json
+from dictionary import WrapperDictionary
 
 urls = (
     '/', 'REST',
+	'/feedback', 'Feedback',
 )
 
 class REST:        
 	def GET(self):
-		param = web.input(query="")
+		param = web.input(query="", getTopics="", getDictionary="")
 		
 		query = param.query
-		if query == "":
-			return "" 
+		topics = param.getTopics
+		dictionary = param.getDictionary
+		if query != "":
+			model = LDAModel.Instance()
+			topics = model.textquery(query)
 
-		model = LDAModel.Instance()
-		topics = model.textquery(query)
+			result = {"result": topics}
 
-		result = {"result": topics}
+			return json.dumps(result) 
 
-		return json.dumps(result) 
+		if topics != "":
+			model = LDAModel.Instance()
+			topiclist = model.show_topics(int(topics)) 
+			
+			result = {"result": topiclist}
+
+			return json.dumps(result) 
+
+		if dictionary != "":
+			map = WrapperDictionary.Instance().token2id
+			result = {"result": map}
+
+			return json.dumps(result) 
+
+
+		return "" 
 	
 	def POST(self):
 		self.GET()
+	
+class Feedback:
+	def GET(self):
+		param = web.input(word="")
+
+		word = param.word
+		if word != "":
+			words = [ x.strip() for x in open("top1000.txt").readlines() ]
+			for w in words:
+				WrapperDictionary.Instance().add_word(w)
+			return "OK"
+
+			response = WrapperDictionary.Instance().add_word(word)
+			if response:
+				return "OK"
+			else:
+				return "ADD_ERROR"
+		
+		return "";
+	
+	def POST(self):
+		return self.GET()
 
 def run():
 	app = web.application(urls, globals())
