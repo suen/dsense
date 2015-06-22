aae78f18a1a305aa97d5d4263b2f878be0209e75
Surendra <suren@surendradangol.com.np>
2015-06-22 23:49:34 +0200

    Transformation into class, 2 process thread, one network thread

--- databot/publish_to_redis.py
+++ databot/publish_to_redis.py
@@ -5,52 +5,95 @@ from http_crawler import TwitterAPIClient
 from threading import Thread
 
 
+class PublishToRedis:
+	def __init__(self):
+		self.redisclient = redis.StrictRedis()
+		self.tc = TwitterAPIClient()	
 
-def process():
-	while True:
-		if len(buffer) == 0:
-			time.sleep(1)
-			continue
+		self.buffer2 = []
+		self.buffer1 = []
 
-		msg = buffer[0]
-		buffer = buffer[1:]
+		self.pthread = Thread(target=self.thread1)
+		self.pthread.setDaemon(True)
+		self.pthread.start()
 
+		self.rthread = Thread(target=self.thread2)
+		self.rthread.setDaemon(True)
+		self.rthread.start()
+	
+	def run(self):
+		time1 = time.time()
+
+		count = 0
+		while True:
+			resp = self.tc.fetchPublicStreamSample()
+			for r in resp:
+				
+				
+				if count % 2 == 0:
+					self.buffer1.append(r)
+				else:
+					self.buffer2.append(r)
+
+				count += 1
+				if count % 500 == 0:
+					time2 = time.time()
+					delta = time2 - time1
+					rate = 500 / delta
+					print "%d tweets published, Rate: %f tweets per second"%(count,rate)
+					time1 = time2
+			
+			print "Stream failed, retrying in 10 seconds"
+			time.sleep(10) 
+
+	def thread1(self):
+		count = 0
+		while True:
+			if len(self.buffer1) == 0:
+				time.sleep(0.1)
+				continue
+
+			msg = self.buffer1[0]
+			self.buffer1 = self.buffer1[1:]
+
+			self.process(msg)
+			count += 1
+
+			if count % 500 == 0:
+				print "Thread1: %d processed"%count
+
+	def thread2(self):
+		count = 0
+		while True:
+			if len(self.buffer2) == 0:
+				time.sleep(0.1)
+				continue
+
+			msg = self.buffer2[0]
+			self.buffer2 = self.buffer2[1:]
+			
+			self.process(msg)
+
+			count += 1
+
+			if count % 500 == 0:
+				print "Thread2: %d processed"%count
+
+
+	def process(self, msg):
 		msg = json.loads(msg)
 		if not msg.has_key('lang') or msg['lang'] != "en":
-			continue
+			return	
 
 		text = msg['text']
 		if len(text.split()) <= 4:
-			continue
+			return	
 		
 		plaintweet = {"text": text, "id": msg['id'] }
 
-		redisclient.publish("twitter-realtime", json.dumps(plaintweet))
+		self.redisclient.publish("twitter-realtime", json.dumps(plaintweet))
+
 	
-
-pthread = Thread(target=process)
-pthread.setDaemon(True)
-
-redisclient = redis.StrictRedis()
-tc = TwitterAPIClient()	
-
-count = 0
-buffer = []
-time1 = time.time()
-pthread.start()
-while True:
-	resp = tc.fetchPublicStreamSample()
-	for r in resp:
-		
-		buffer.append(r)
-
-		count += 1
-		if count % 500 == 0:
-			time2 = time.time()
-			delta = time2 - time1
-			rate = 500 / delta
-			print "%d tweets published, Rate: %f tweets per second"%(count,rate)
-			time1 = time2
-	
-	print "Stream failed, retrying in 10 seconds"
-	time.sleep(10)
+if __name__ == "__main__":
+	ptr = PublishToRedis()	
+	ptr.run()
